d63db7184ba52f96e2ec01fe47197d7d7c7e3440
Biswyan Mazumder <biswayanmazumder2006@gmail.com>
2025-08-30 23:28:15 +0530

    language translation semi done

--- SIH2025/src/App.jsx
+++ SIH2025/src/App.jsx
@@ -1,24 +1,33 @@
 import { Outlet } from 'react-router-dom'
 import Navbar from './components/Navbar.jsx'
 import { ThemeProvider } from './context/ThemeContext'
-import { LoadingProvider } from './context/LoadingContext'
+import { LanguageProvider, useLanguage } from './context/LanguageContext'
+// import { LoadingProvider } from './context/LoadingContext'
 import './App.css'
+
+function AppContent() {
+  const { translate } = useLanguage();
+  
+  return (
+    <ThemeProvider>
+      <div className="min-h-dvh flex flex-col transition-colors duration-300">
+        <Navbar />
+        <main className="flex-1">
+          <Outlet />
+        </main>
+          <footer className="border-t text-xs text-center py-4 text-gray-500 dark:text-gray-400 dark:border-gray-700">
+            {translate('footer', { year: new Date().getFullYear() })}
+          </footer>
+      </div>
+    </ThemeProvider>
+  )
+}
 
 function App() {
   return (
-    <ThemeProvider>
-      <LoadingProvider>
-        <div className="min-h-dvh flex flex-col transition-colors duration-300">
-          <Navbar />
-          <main className="flex-1">
-            <Outlet />
-          </main>
-          <footer className="border-t text-xs text-center py-4 text-gray-500 dark:text-gray-400 dark:border-gray-700">
-            © {new Date().getFullYear()} TransitTrack
-          </footer>
-        </div>
-      </LoadingProvider>
-    </ThemeProvider>
+    <LanguageProvider>
+      <AppContent />
+    </LanguageProvider>
   )
 }
 
